fd7227d8ede1ae3604f9c866b27ece447d3d441b
Ahmed Mohamed Aboutaleb <ahmed.abotalb94@gmail.com>
2022-04-11 00:32:18 +0200

    :rocket:  Deploy stuff.
     Deploy stuff.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,13 +15,6 @@ class MyDocument extends Document {
     render() {
         return (
             <Html lang='en'>
-                <Head>
-                    <script
-                        async
-                        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}`}
-                        crossOrigin='anonymous'
-                    />
-                </Head>
                 <body data-what-you-are-looking-at='👀'>
                     <Main />
                     <NextScript />
