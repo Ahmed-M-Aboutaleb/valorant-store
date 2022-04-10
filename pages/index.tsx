9b9db71db9d9e482ac9c79289944a3a48103b8f2
Ahmed Mohamed Aboutaleb <ahmed.abotalb94@gmail.com>
2022-04-10 11:42:22 +0200

    :ambulance: Fixed google adsense tag
    Fixed google adsense tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,7 +50,7 @@ export default function Home() {
                 <script
                     async
                     src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4638221760277043'
-                    crossorigin='anonymous'
+                    crossOrigin='anonymous'
                 ></script>
             </Head>
             <main className={styles.main}>
