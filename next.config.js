c17135769b640bdb6884349374e7195532af7f5a
Ahmed Mohamed Aboutaleb <ahmed.abotalb94@gmail.com>
2022-04-13 10:26:38 +0200

    :sparkles: Website is PWA now
     Website is PWA now

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa');
+
+const nextConfig = withPWA({
     reactStrictMode: true,
     images: {
-        domains: ['media.valorant-api.com'],
+        domains: ['media.valorant-api.com', 'valorant-store.xyz'],
     },
-};
+    pwa: {
+        disable: process.env.NODE_ENV === 'development',
+        dest: 'public',
+    },
+});
 
 module.exports = nextConfig;
